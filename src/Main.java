bdec6b81326cc8c6aed90fd73aa4702b5ce7eaab
Jairinka <ipaschenko@gmail.com>
2018-10-03 18:52:59 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=1;
+        short b=2;
+        long c=5000000;
+        double d=0.8;
+        float e=4.7f;
+
+        System.out.println(a+b);
+        System.out.println(c-a);
+        System.out.println(b*c);
+        System.out.println((float)a/b);
+
+        System.out.println("Hello, Word!");
+        String hm="Home work has been done";
+        System.out.println(hm);
+
+    }
+}
